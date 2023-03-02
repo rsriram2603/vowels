f8dbd1d502ff61fa09693cbbc11618ab3c26992a
rsriram2603 <rsriram830@gmail.com>
2023-03-02 15:06:52 +0530

    new file add

---
+++ main.js
@@ -0,0 +1,40 @@
+// console.log("--------------vowel----------------");
+// function call()
+// {
+	// var string=prompt("enter a string :"),count=0;
+	
+	// for (i=0;i<string.length;i++)
+	// {
+		// if(string[i]=="a"||string[i]=="e"||string[i]=="i"||string[i]=="o"||string[i]=="u")
+		// {
+			// count++;
+		// }
+		
+	// }
+		// console.log(count);
+// }
+// call();
+// console.log("--------------------odd number------------------");
+// var a=[1,2,3,4,5,6,7,8];
+// function odd(){
+// {
+	// for(i=0;i<=a.length;i++)
+	// {
+		// if(a[i]%2==1)
+		// {
+			// console.log(a[i]);
+		// }
+	// }
+// }
+// odd();
+console.log("------------------pattern program-----------------------");
+console.log("<br>");
+var n=10;
+for (i=1;i<=n;i++)
+{
+	for(j=1;j<=i;j++)
+	{
+		console.log(i);
+	}
+	console.log("<br>");
+}
